5d3ce348a11e3c03359e385187c99a9368a38f08
ArturHamannRonconi <arturhronconi@gmail.com>
2022-01-04 18:22:56 -0300

    add setupApp

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+async function setupApp(app: INestApplication) {
+  app.setGlobalPrefix('api/v1');
+}
 
 async function bootstrap() {
   const { PORT } = process.env;
   const app = await NestFactory.create(AppModule);
+
+  setupApp(app);
+
   await app.listen(PORT);
 }
 
